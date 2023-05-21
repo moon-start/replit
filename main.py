19dc39ca8b357c41196e2797d1d046f26a6d271f
moon-start <53564780+moon-start@users.noreply.github.com>
2023-05-21 17:36:35 +0000

    存檔
    OK

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(123456789)
+#######################################################################
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+port = 80
+
+httpd = HTTPServer(('', port), CGIHTTPRequestHandler)
+print("Starting simple_httpd on port: " + str(httpd.server_port))
+httpd.serve_forever()
+########################
